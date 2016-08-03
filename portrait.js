57855f9cc9047db28e4ebf2b388511218006e0d8
MingjiaNi <m6ni@uwaterloo.ca>
2016-08-02 20:18:50 -0400

    Workon the input field

--- portrait.js
+++ portrait.js
@@ -1,3 +1,20 @@
+//auto resize the textarea
+$(document).one('focus.textarea', '.autoExpand', function(){
+	var savedValue = this.value;
+	this.value = '';
+	this.baseScrollHeight = this.scrollHeight;
+	this.value = savedValue;
+})
+.on('input.textarea', '.autoExpand', function(){
+	var minRows = this.getAttribute('data-min-rows')|0,
+		 rows;
+	this.rows = minRows;
+	console.log(this.scrollHeight , this.baseScrollHeight);
+	rows = Math.ceil((this.scrollHeight - this.baseScrollHeight) / 20);
+	this.rows = minRows + rows;
+});
+
+
 function toggleSideBar(){
 	var isToggle = $('#sidebar').is(":visible");
 	console.log(isToggle);
@@ -20,3 +37,8 @@ function toggleSideBar(){
     });
 }
 
+function showNoteContent(){
+	$('#noteContent').toggle(300,"swing");
+	$('#note').attr("placeholder","Take a note…");
+	$('#noteTitle').empty();
+}
